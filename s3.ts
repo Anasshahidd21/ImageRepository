ceff9657421fce7d1f6ab455358442bfe919cbce
Anas Shahid <muhammad.shahid@ericsson.com>
2021-09-27 14:50:10 -0400

    removed sensitive details

--- s3.ts
+++ s3.ts
@@ -1,27 +1,4 @@
 require("dotenv").config();
-import { ACCESS_KEY, SECRET_KEY, S3_BUCKET_REGION } from "./s3details";
-
-// const s3 = new aws.S3({
-//   accessKeyId: ACCESS_KEY,
-//   secretAccessKey: SECRET_KEY,
-//   region: S3_BUCKET_REGION,
-// });
-
-// const upload = multer({
-//   storage: multerS3({
-//     s3,
-//     bucket: "myshopifybucket",
-//     metadata: function (req, file, cb) {
-//       cb(null, { fieldName: file.fieldname });
-//     },
-//     key: function (req, file, cb) {
-//       cb(null, `image-${Date.now()}.jpeg`);
-//     },
-//   }),
-// });
-
-// export default upload;
-
 import aws from "aws-sdk";
 import multer from "multer";
 import multerS3 from "multer-s3";
